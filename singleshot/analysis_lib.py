55d5214e863c3a2b210fdf7ef11693fdc48811b5
sslabstanford <40874785+sslabstanford@users.noreply.github.com>
2025-03-04 12:10:27 -0800

    cloud_temperature.py done using the class structure, replacing MOT_TOF_single.py, MOT_TOF_fitting.py, MOT_temperature_plotting.py

--- singleshot/analysis_lib.py
+++ singleshot/analysis_lib.py
@@ -19,7 +19,7 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import csv
-
+import scipy.optimize as opt
 
 @dataclass
 class ImagingCamera:
@@ -58,6 +58,10 @@ class ImagingSetup:
     @property
     def magnification(self):
         return self.imaging_f / self.objective_f
+
+    @property
+    def pixel_size_before_maginification(self):
+        return  self.camera.pixel_size/self.magnification
 
     @property
     def solid_angle_fraction(self):
@@ -112,8 +116,14 @@ manta_path = ImagingSetup(
 
 
 class BulkGasAnalysis:
-    scattering_rate = 2 * np.pi * 5.2227e+6
+    scattering_rate = 2 * np.pi * 5.2227e+6 # rad/s
     """ Cesium scattering rate """
+
+    m = 2.20694650e-25
+    """ Cesium mass in kg"""
+
+    kB = 1.3806503e-23
+    """J/K Boltzman constant"""
 
     def __init__(self, imaging_setup: ImagingSetup, exposure_time, atoms_roi, bkg_roi):
         """
@@ -143,7 +153,7 @@ class BulkGasAnalysis:
             exposure_time,
         )
 
-        self.images, self.run_number = self.load_image_in_lyse()
+        self.images, self.run_number, self.globals = self.load_image_in_lyse()
         self.mot_image, self.background_image, self.sub_image = self.get_image_bkg_sub()
         self.roi_atoms, self.roi_bkg = self.get_images_roi()
 
@@ -166,16 +176,17 @@ class BulkGasAnalysis:
             h5_path = df.filepath.iloc[-1]
 
         self.h5_path = h5_path
+        self.folder_path = '\\'.join(h5_path.split('\\')[0:-1])
 
         with h5py.File(h5_path, mode='r+') as f:
-            globals = hz.attributesToDictionary(f['globals'])
+            globals = hz.getGlobalsFromFile(h5_path)
             info_dict = hz.getAttributeDict(f)
             images = hz.datasetsToDictionary(f['manta419b_mot_images'], recursive=True)
             run_number = f.attrs['run number']
             # self.images = images
             # self.run_number = f.attrs['run number']
 
-        return images, run_number
+        return images, run_number, globals
 
 
 
@@ -241,6 +252,79 @@ class BulkGasAnalysis:
 
         return atom_number
 
+    def gauss2D(self,x, amplitude, mux, muy, sigmax, sigmay, rotation, offset):
+        """
+        2D Gaussian, see: https://en.wikipedia.org/wiki/Gaussian_function
+        Parameters:
+        -----------
+        x: tuple of 2 arrays, x[0] for x and x[1] for y
+        amplitude: amplitude of the gaussian, float
+        mux: center of the gaussian in x, float
+        muy: center of the gaussian in y, float
+        sigmax: width of the gaussian in x, float
+        sigmay: width of the gaussian in y, float
+        rotation: rotation of the gaussian, float
+        offset: offset of the gaussian, float
+
+        Returns
+        -------
+        G: array like shape[1,:],
+          1d ravel of the gaussian
+        """
+        assert len(x) == 2
+        X = x[0]
+        Y = x[1]
+        A = (np.cos(rotation)**2)/(2*sigmax**2) + (np.sin(rotation)**2)/(2*sigmay**2)
+        B = (np.sin(rotation*2))/(4*sigmay**2) - (np.sin(2*rotation))/(4*sigmax**2)
+        C = (np.sin(rotation)**2)/(2*sigmax**2) + (np.cos(rotation)**2)/(2*sigmay**2)
+        G = amplitude*np.exp(-(A * (X - mux) ** 2 + 2 * B * (X - mux) * (Y - muy) + C * (Y - muy) ** 2)) + offset  # + slopex * X + slopey * Y + offset
+        return G.ravel()  # np.ravel() Return a contiguous flattened array.
+
+    def get_atom_temperature(self,):
+        """
+        measure the temperature of atom through time of flight
+        the waist of the cloud is determined through 2D gaussian fitting
+        T = m*(w/t)^2 / k_B
+
+        Returns
+        -------
+        time_of_flight: float
+        atom_number_gaussian: float atom number through gaussian fitting
+        guassian_waist: array_like, shape [2]
+        temperature: array_like, shape [2]
+        """
+        # Creating a grid for the Gaussian fit
+        [roi_x, roi_y] = self.atoms_roi
+        x_size = roi_x[1]-roi_x[0]
+        y_size = roi_y[1]-roi_y[0]
+        x = np.linspace(0, x_size-1, x_size)
+        y = np.linspace(0, y_size-1, y_size)
+        x, y = np.meshgrid(x, y)
+
+        # Fitting the 2d Gaussian
+        initial_guess = np.array([
+            np.max(self.roi_atoms),
+            x_size/2,
+            y_size/2,
+            x_size/2,
+            y_size/2,
+            0,
+            np.min(self.roi_atoms)])
+
+        popt, pcov = opt.curve_fit(self.gauss2D, (y, x), self.roi_atoms.ravel(), p0=initial_guess)
+        atom_number_gaussian = np.abs(2 * np.pi * popt[0] * popt[3] * popt[4] / self.counts_per_atom)
+        sigma = np.sort(np.abs([popt[3], popt[4]]))  # gaussian waiast in pixel, [short axis, long axis]
+        gaussian_waist = np.array(sigma)*self.imaging_setup.pixel_size_before_maginification# convert from pixel to distance m
+
+        time_of_flight = self.globals['bm_tof_imaging_delay']
+        if self.globals['do_dipole_trap_tof_check'] == True:
+            time_of_flight = self.globals['img_tof_imaging_delay']
+        temperature = self.m / self.kB * (gaussian_waist/time_of_flight)**2
+
+        self.save_atom_temperature(atom_number_gaussian, time_of_flight, gaussian_waist, temperature)
+
+        return time_of_flight, atom_number_gaussian, gaussian_waist, temperature
+
     def plot_images(self, image_scale = 100):
         """
         Parameters
@@ -263,8 +347,8 @@ class BulkGasAnalysis:
             ax.set_xlabel('x [px]')
             ax.set_ylabel('y [px]')
         for ax in axs[1]:
-            ax.set_xlabel('x [um]')
-            ax.set_ylabel('y [um]')
+            ax.set_xlabel('x [m]')
+            ax.set_ylabel('y [m]')
 
 
 
@@ -277,11 +361,11 @@ class BulkGasAnalysis:
         pos = ax_bkg_raw.imshow(background_image, **raw_img_color_kw)
         fig.colorbar(pos, ax=ax_bkg_raw)
 
-        pixel_size_after_maginification = self.imaging_setup.camera.pixel_size*self.imaging_setup.magnification
+        pixel_size_before_maginification = self.imaging_setup.pixel_size_before_maginification
         ax_mot_roi.set_title('mot ROI')
         pos = ax_mot_roi.imshow(
             roi_atoms,
-            extent=np.array([roi_x[0], roi_x[1], roi_y[0], roi_y[1]])*pixel_size_after_maginification,
+            extent=np.array([roi_x[0], roi_x[1], roi_y[0], roi_y[1]])*pixel_size_before_maginification,
             **raw_img_color_kw,
         )
         fig.colorbar(pos, ax=ax_mot_roi)
@@ -291,10 +375,10 @@ class BulkGasAnalysis:
             roi_bkg,
             vmin=-10,
             vmax=10,
-            extent=np.array([roi_x_bkg[0], roi_x_bkg[1], roi_y_bkg[0], roi_y_bkg[1]])*pixel_size_after_maginification, #factor: px*mag
+            extent=np.array([roi_x_bkg[0], roi_x_bkg[1], roi_y_bkg[0], roi_y_bkg[1]])*pixel_size_before_maginification, #factor: px*mag
         )
         fig.colorbar(pos, ax=ax_bkg_roi)
-
+        fig.savefig(self.folder_path+'\\atom_cloud.png')
         return
 
     def save_atom_number(self, atom_number):
@@ -303,17 +387,33 @@ class BulkGasAnalysis:
         if run number is 0, it will create a new file
         otherwise it will append
         """
-        h5_path = self.h5_path
         run_number = self.run_number
 
-        folder_path = '\\'.join(h5_path.split('\\')[0:-1])
-        count_file_path = folder_path+'\\data.csv'
-        if run_number == 0:
-            with open(count_file_path, 'w') as f_object:
+        count_file_path = self.folder_path+'\\data.csv'
+        file_open_mode = 'w' if run_number == 0 else 'a'
+        with open(count_file_path, file_open_mode) as f_object:
                 f_object.write(f'{atom_number}\n')
-        else:
-            with open(count_file_path, 'a') as f_object:
-                f_object.write(f'{atom_number}\n')
+        return
+
+    def save_atom_temperature(self, atom_number, time_of_flight, gaussian_waist, atom_temperature):
+        """
+        save atom temperature to data.csv
+        if run number is 0, it will create a new file
+        otherwise it will append
+        """
+        run_number = self.run_number
+
+
+        count_file_path = self.folder_path+'\\data.csv'
+        file_open_mode = 'w' if run_number == 0 else 'a'
+        with open(count_file_path, file_open_mode) as f_object:
+            f_object.write(f'{atom_number},'
+                           f'{time_of_flight},'
+                           f'{gaussian_waist[0]},'
+                           f'{gaussian_waist[1]},'
+                           f'{atom_temperature[0]},'
+                           f'{atom_temperature[1]}\n')
+
         return
 
     def load_atom_number(self,):
@@ -348,5 +448,65 @@ class BulkGasAnalysis:
 
         ax.grid(color='0.7', which='major')
         ax.grid(color='0.9', which='minor')
+        fig.savefig(self.folder_path+'\\atom_number.png')
 
+    def load_atom_temperature(self,):
+        """
+        load the atom number that is already saved in data.csv
+
+        Returns
+        -------
+        counts: float
+
+        """
+        h5_path = self.h5_path
+        folder_path = '\\'.join(h5_path.split('\\')[0:-1])
+        count_file_path = folder_path+'\\data.csv'
+        with open(count_file_path, newline='') as csvfile:
+            matrix = [list(map(float, row)) for row in csv.reader(csvfile)]
+        matrix = np.array(matrix)
+        time_of_flight, waist_x, waist_y, temperature_x, temperature_y = [matrix[:,i] for i in np.arange(1,6)]
+        return time_of_flight, waist_x, waist_y, temperature_x, temperature_y
+
+    def plot_atom_temperature(self,):
+        """
+        plot atom temperature from data.csv vs the shot number
+
+        """
+        time_of_flight, waist_x, waist_y, temperature_x, temperature_y = self.load_atom_temperature()
+        fig, ax = plt.subplots(constrained_layout=True)
+
+        ax.plot(np.array(temperature_x)*1e6, label='X temperature')
+        ax.plot(np.array(temperature_y)*1e6, label='Y temperature')
+        ax.set_xlabel('Shot number')
+        ax.set_ylabel('Temperature (uK)')
+        ax.legend()
+
+        ax.grid(color='0.7', which='major')
+        ax.grid(color='0.9', which='minor')
+        fig.savefig(self.folder_path+'\\temperature_single_shot.png')
+
+        # linear fit to extrac the velocity of the atoms through sphereical expansion
+        slope_x, intercept_x = np.polyfit(time_of_flight**2, np.array(waist_x)**2, 1)
+        slope_y, intercept_y = np.polyfit(time_of_flight**2, np.array(waist_y)**2, 1)
+        slope = np.array([slope_x, slope_y])
+        T = slope*self.m/self.kB*1e6
+
+        t_plot = np.arange(0,max(time_of_flight)+1e-3,1e-3)
+        fig2, ax2 = plt.subplots(constrained_layout=True)
+        ax2.plot(t_plot**2*1e6 , (slope_x*t_plot**2 + intercept_x)*1e12, label=rf'major axis fit, $T = {T[0]:.3f}$ $\mu$K')
+        ax2.plot(t_plot**2*1e6 , (slope_y*t_plot**2 + intercept_y)*1e12, label=rf'minor axis fit, $T = {T[1]:.3f}$ $\mu$K')
+        ax2.plot(time_of_flight**2*1e6, np.array(waist_x)**2*1e12, 'oC0', label ='data x')
+        ax2.plot(time_of_flight**2*1e6, np.array(waist_y)**2*1e12, 'oC1', label ='data y')
+        ax2.set_xlabel('Shot number')
+        ax2.set_xlabel(r'Time$^2$ (ms$^2$)')
+        ax2.set_ylabel(r'$\sigma^2$ ($\mu$m$^2$)')
+        ax2.legend()
+        ax2.set_ylim(ymin=0)
+        ax2.tick_params(axis = 'x')
+        ax2.tick_params(axis = 'y')
+
+        ax2.grid(color='0.7', which='major')
+        ax2.grid(color='0.9', which='minor')
+        fig2.savefig(self.folder_path+'\\temperature_fit.png')
 
---
+++ singleshot/cloud_temperature.py
@@ -0,0 +1,20 @@
+from analysis_lib import BulkGasAnalysis, manta_path
+
+roi_x = [800, 1100]#roi_x = [850, 1250] # Region of interest of X direction
+roi_y = [900, 1200] #[750, 1150] # Region of interest of Y direction
+roi_x_bkg = [1900, 2400] # Region of interest of X direction
+roi_y_bkg= [1900, 2400] # Region of interest of Y direction
+
+t_expo = 1e-3 #s, exposre time
+
+bulk_gas_analysis_obj = BulkGasAnalysis(
+    imaging_setup=manta_path,
+    exposure_time=t_expo,
+    atoms_roi=[roi_x, roi_y],
+    bkg_roi=[roi_x_bkg, roi_y_bkg],
+)
+
+bulk_gas_analysis_obj.get_atom_temperature()
+bulk_gas_analysis_obj.plot_images()
+bulk_gas_analysis_obj.plot_atom_number()
+bulk_gas_analysis_obj.plot_atom_temperature()
