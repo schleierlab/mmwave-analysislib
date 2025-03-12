2858dfe59ceb6c359b74ad85c45cfade3b30babc
sslabstanford <40874785+sslabstanford@users.noreply.github.com>
2025-03-12 12:11:01 -0700

    Changed plotting classes to refer to a common PlotConfig class for shared matplotlib params that we want to set globally

--- singleshot/common/gas_plot.py
+++ singleshot/common/gas_plot.py
@@ -1,19 +1,14 @@
-class BulkGasPlotter:
-    def __init__(self, bulk_gas_analyzer):
-        self.bulk_gas_analyzer = bulk_gas_analyzer
+from .plot_config import PlotConfig
 
-    def plot_images(self, raw_image_scale = 100, roi_image_scale = 100):
+class BulkGasPlotter:
+    def __init__(self, bulk_gas_analyzer, plot_config: PlotConfig = None):
+        self.bulk_gas_analyzer = bulk_gas_analyzer
+        self.plot_config = plot_config or PlotConfig()
+
+    def plot_images(self):
         """
         plot the images of raw image/background image full frame and background subtracted images in rois
         and also save the images in the folder path
-
-        Parameters
-        ----------
-        raw_image_scale: float
-            scale of raw image in the plot
-        roi_image_scale: float
-            scale of roi image in the plot
-
         """
         atom_image = self.atom_image
         background_image = self.background_image
@@ -22,69 +17,72 @@ class BulkGasPlotter:
         [roi_x, roi_y] = self.atoms_roi
         [roi_x_bkg, roi_y_bkg] = self.background_roi
 
-        fig, axs = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
+        fig, axs = plt.subplots(nrows=2, ncols=2, 
+                               figsize=self.plot_config.figure_size,
+                               constrained_layout=self.plot_config.constrained_layout)
         (ax_atom_raw, ax_bkg_raw), (ax_bkg_roi, ax_atom_roi) = axs
         for ax in axs[0]:
             ax.set_xlabel('x [px]')
             ax.set_ylabel('y [px]')
+            ax.tick_params(axis='both', which='major', labelsize=self.plot_config.label_font_size)
         for ax in axs[1]:
             ax.set_xlabel('x [m]')
             ax.set_ylabel('y [m]')
+            ax.tick_params(axis='both', which='major', labelsize=self.plot_config.label_font_size)
 
-
-
-        raw_img_color_kw = dict(cmap='viridis', vmin=0, vmax= raw_image_scale)
-        ax_atom_raw.set_title('Raw, with atom')
+        raw_img_color_kw = dict(cmap=self.plot_config.colormap, vmin=0, vmax=self.plot_config.raw_image_scale)
+        ax_atom_raw.set_title('Raw, with atom', fontsize=self.plot_config.title_font_size)
         pos = ax_atom_raw.imshow(atom_image, **raw_img_color_kw)
-        fig.colorbar(pos, ax=ax_atom_raw)
+        fig.colorbar(pos, ax=ax_atom_raw).ax.tick_params(labelsize=self.plot_config.label_font_size)
 
-        ax_bkg_raw.set_title('Raw, no atom')
+        ax_bkg_raw.set_title('Raw, no atom', fontsize=self.plot_config.title_font_size)
         pos = ax_bkg_raw.imshow(background_image, **raw_img_color_kw)
-        fig.colorbar(pos, ax=ax_bkg_raw)
+        fig.colorbar(pos, ax=ax_bkg_raw).ax.tick_params(labelsize=self.plot_config.label_font_size)
 
         pixel_size_before_maginification = self.imaging_setup.pixel_size_before_maginification
-        ax_atom_roi.set_title('Atom ROI')
-        roi_img_color_kw = dict(cmap='viridis', vmin=0, vmax= roi_image_scale)
+        ax_atom_roi.set_title('Atom ROI', fontsize=self.plot_config.title_font_size)
+        roi_img_color_kw = dict(cmap=self.plot_config.colormap, vmin=0, vmax=self.plot_config.roi_image_scale)
         pos = ax_atom_roi.imshow(
             roi_atoms,
             extent=np.array([roi_x[0], roi_x[1], roi_y[0], roi_y[1]])*pixel_size_before_maginification,
             **roi_img_color_kw,
         )
-        fig.colorbar(pos, ax=ax_atom_roi)
+        fig.colorbar(pos, ax=ax_atom_roi).ax.tick_params(labelsize=self.plot_config.label_font_size)
 
-        ax_bkg_roi.set_title('Background ROI')
+        ax_bkg_roi.set_title('Background ROI', fontsize=self.plot_config.title_font_size)
         pos = ax_bkg_roi.imshow(
             roi_bkg,
             vmin=-10,
             vmax=10,
-            extent=np.array([roi_x_bkg[0], roi_x_bkg[1], roi_y_bkg[0], roi_y_bkg[1]])*pixel_size_before_maginification, #factor: px*mag
+            extent=np.array([roi_x_bkg[0], roi_x_bkg[1], roi_y_bkg[0], roi_y_bkg[1]])*pixel_size_before_maginification,
+            cmap=self.plot_config.colormap
         )
-        fig.colorbar(pos, ax=ax_bkg_roi)
+        fig.colorbar(pos, ax=ax_bkg_roi).ax.tick_params(labelsize=self.plot_config.label_font_size)
         fig.savefig(self.folder_path+'\\atom_cloud.png')
         return
 
-    def plot_atom_number(self,):
+    def plot_atom_number(self):
         """
         To run this function, we need to run get_atom_number first and have data.csv in the same folder
         plot atom number from data.csv vs the shot number and also save the image in the folder path
         this plot all the shots in the same queue start with run number = 0
         so that we can see the trend
-
         """
         counts = self.load_atom_number()
-        fig, ax = plt.subplots(constrained_layout=True)
+        fig, ax = plt.subplots(figsize=self.plot_config.figure_size,
+                              constrained_layout=self.plot_config.constrained_layout)
 
         ax.plot(counts,'o-')
-        ax.set_xlabel('Shot number')
-        ax.set_ylabel('atom count')
+        ax.set_xlabel('Shot number', fontsize=self.plot_config.label_font_size)
+        ax.set_ylabel('atom count', fontsize=self.plot_config.label_font_size)
+        ax.tick_params(axis='both', which='major', labelsize=self.plot_config.label_font_size)
 
-        ax.grid(color='0.7', which='major')
-        ax.grid(color='0.9', which='minor')
+        ax.grid(color=self.plot_config.grid_color_major, which='major')
+        ax.grid(color=self.plot_config.grid_color_minor, which='minor')
         fig.savefig(self.folder_path+'\\atom_number.png')
-
         return
 
-    def plot_atom_temperature(self,):
+    def plot_atom_temperature(self):
         """
         plot atom temperature from data.csv vs the shot number
         fitting more accurate temperature with different time of flight through linear fit
@@ -93,16 +91,18 @@ class BulkGasPlotter:
         save all the plots in the folder path
         """
         time_of_flight, waist_x, waist_y, temperature_x, temperature_y = self.load_atom_temperature()
-        fig, ax = plt.subplots(constrained_layout=True)
+        fig, ax = plt.subplots(figsize=self.plot_config.figure_size,
+                              constrained_layout=self.plot_config.constrained_layout)
 
         ax.plot(np.array(temperature_x)*1e6, label='X temperature')
         ax.plot(np.array(temperature_y)*1e6, label='Y temperature')
-        ax.set_xlabel('Shot number')
-        ax.set_ylabel('Temperature (uK)')
-        ax.legend()
+        ax.set_xlabel('Shot number', fontsize=self.plot_config.label_font_size)
+        ax.set_ylabel('Temperature (uK)', fontsize=self.plot_config.label_font_size)
+        ax.tick_params(axis='both', which='major', labelsize=self.plot_config.label_font_size)
+        ax.legend(fontsize=self.plot_config.label_font_size)
 
-        ax.grid(color='0.7', which='major')
-        ax.grid(color='0.9', which='minor')
+        ax.grid(color=self.plot_config.grid_color_major, which='major')
+        ax.grid(color=self.plot_config.grid_color_minor, which='minor')
         fig.savefig(self.folder_path+'\\temperature_single_shot.png')
 
         if self.run_number >= 2:
@@ -124,7 +124,8 @@ class BulkGasPlotter:
             T_lst = slope_uncertainty_lst*self.m/self.kB*1e6
 
             t_plot = np.arange(0,max(time_of_flight)+1e-3,1e-3)
-            fig2, ax2 = plt.subplots(constrained_layout=True)
+            fig2, ax2 = plt.subplots(figsize=self.plot_config.figure_size,
+                                    constrained_layout=self.plot_config.constrained_layout)
             for slope,intercept,T in zip(slope_lst,intercept_lst,T_lst):
                 ax2.plot(t_plot**2*1e6 , (slope*t_plot**2 + intercept)*1e12, label=rf'$T = {T:LS}$ $\mu$K')
 
@@ -133,16 +134,14 @@ class BulkGasPlotter:
                 ax2.plot(time_of_flight**2*1e6, np.array(waist)**2*1e12, 'oC'+str(i), label = 'data')
                 i+=1
 
-            ax2.set_xlabel('Shot number')
-            ax2.set_xlabel(r'Time$^2$ (ms$^2$)')
-            ax2.set_ylabel(r'$\sigma^2$ ($\mu$m$^2$)')
-            ax2.legend()
+            ax2.set_xlabel(r'Time$^2$ (ms$^2$)', fontsize=self.plot_config.label_font_size)
+            ax2.set_ylabel(r'$\sigma^2$ ($\mu$m$^2$)', fontsize=self.plot_config.label_font_size)
+            ax2.tick_params(axis='both', which='major', labelsize=self.plot_config.label_font_size)
+            ax2.legend(fontsize=self.plot_config.label_font_size)
             ax2.set_ylim(ymin=0)
-            ax2.tick_params(axis = 'x')
-            ax2.tick_params(axis = 'y')
 
-            ax2.grid(color='0.7', which='major')
-            ax2.grid(color='0.9', which='minor')
+            ax2.grid(color=self.plot_config.grid_color_major, which='major')
+            ax2.grid(color=self.plot_config.grid_color_minor, which='minor')
             fig2.savefig(self.folder_path+'\\temperature_fit.png')
         return
 
@@ -154,10 +153,10 @@ class BulkGasPlotter:
         the data will be plotted against the first parameter for now
 
         this can be modified for general use with multiple repetitions and multiple parameters
-
         """
         amplitude = self.load_atom_number()
-        fig, ax = plt.subplots(constrained_layout=True)
+        fig, ax = plt.subplots(figsize=self.plot_config.figure_size,
+                              constrained_layout=self.plot_config.constrained_layout)
 
         for key, value in self.params.items():
             para = eval(value[0])
@@ -177,9 +176,10 @@ class BulkGasPlotter:
             raise NotImplementedError("2 scanning parameters is not supported yet")
 
         ax.plot(para, amplitude_resize, '-o')
-        ax.set_xlabel(label_string)
-        ax.set_ylabel('amplitude')
-        ax.grid(color='0.7', which='major')
-        ax.grid(color='0.9', which='minor')
+        ax.set_xlabel(label_string, fontsize=self.plot_config.label_font_size)
+        ax.set_ylabel('amplitude', fontsize=self.plot_config.label_font_size)
+        ax.tick_params(axis='both', which='major', labelsize=self.plot_config.label_font_size)
+        ax.grid(color=self.plot_config.grid_color_major, which='major')
+        ax.grid(color=self.plot_config.grid_color_minor, which='minor')
 
         fig.savefig(self.folder_path+'\\amplitude_vs_parameter.png')
--- singleshot/common/tweezer_plot.py
+++ singleshot/common/tweezer_plot.py
@@ -1,14 +1,15 @@
-class TweezerPlotter:
-    def __init__(self, tweezer_analyzer):
-        self.tweezer_analyzer = tweezer_analyzer
+from .plot_config import PlotConfig
 
-    def plot_images(self, roi_image_scale=150, show_site_roi=True, plot_bkg_roi=True):
+class TweezerPlotter:
+    def __init__(self, tweezer_analyzer, plot_config: PlotConfig = None):
+        self.tweezer_analyzer = tweezer_analyzer
+        self.plot_config = plot_config or PlotConfig()
+
+    def plot_images(self, show_site_roi=True, plot_bkg_roi=True):
         """Plot the analysis results with optional site ROIs and background ROIs.
         
         Parameters
         ----------
-        roi_image_scale : int, default=150
-            Scale factor for ROI image colormaps
         show_site_roi : bool, default=True
             Whether to show site ROI rectangles
         plot_bkg_roi : bool, default=True
@@ -28,27 +29,30 @@ class TweezerPlotter:
 
         # Create figure with enough rows for all shots plus background ROIs
         num_rows = num_of_imgs + (2 if plot_bkg_roi else 0)
-        fig, axs = plt.subplots(nrows=num_rows, ncols=1, figsize=(10, 5*num_rows), constrained_layout=True)
+        fig_height = self.plot_config.figure_size[1] * num_rows
+        fig, axs = plt.subplots(nrows=num_rows, ncols=1, 
+                               figsize=(self.plot_config.figure_size[0], fig_height), 
+                               constrained_layout=self.plot_config.constrained_layout)
         
         # If only one subplot, wrap it in a list for consistent indexing
         if num_rows == 1:
             axs = [axs]
 
-        plt.rcParams.update({'font.size': 14})
-        fig.suptitle('Tweezer Array Imaging Analysis', fontsize=16)
+        plt.rcParams.update({'font.size': self.plot_config.font_size})
+        fig.suptitle('Tweezer Array Imaging Analysis', fontsize=self.plot_config.title_font_size)
 
         # Configure all axes
         for ax in axs:
             ax.set_xlabel('x [px]')
             ax.set_ylabel('y [px]')
-            ax.tick_params(axis='both', which='major', labelsize=12)
+            ax.tick_params(axis='both', which='major', labelsize=self.plot_config.label_font_size)
 
-        roi_img_color_kw = dict(cmap='viridis', vmin=0, vmax=roi_image_scale)
+        roi_img_color_kw = dict(cmap=self.plot_config.colormap, vmin=0, vmax=self.plot_config.roi_image_scale)
 
         # Plot tweezer ROIs for each shot
         for i in range(num_of_imgs):
             ax_tweezer = axs[i]
-            ax_tweezer.set_title(f'Shot {i+1} Tweezer ROI', fontsize=14, pad=10)
+            ax_tweezer.set_title(f'Shot {i+1} Tweezer ROI', fontsize=self.plot_config.title_font_size, pad=10)
             pos = ax_tweezer.imshow(self.roi_atoms[i], **roi_img_color_kw)
             
             if show_site_roi:
@@ -76,12 +80,12 @@ class TweezerPlotter:
                     pc_sig = PatchCollection(rect_sig[i], match_original=True)
                     ax_tweezer.add_collection(pc_sig)
             
-            fig.colorbar(pos, ax=ax_tweezer).ax.tick_params(labelsize=12)
+            fig.colorbar(pos, ax=ax_tweezer).ax.tick_params(labelsize=self.plot_config.label_font_size)
 
         # Plot background ROIs if requested
         if plot_bkg_roi:
             for i in range(min(2, num_of_imgs)):  # Plot up to 2 background ROIs
                 ax_bkg = axs[num_of_imgs + i]
-                ax_bkg.set_title(f'Shot {i+1} Background ROI', fontsize=14, pad=10)
+                ax_bkg.set_title(f'Shot {i+1} Background ROI', fontsize=self.plot_config.title_font_size, pad=10)
                 pos = ax_bkg.imshow(self.roi_bkgs[i], **roi_img_color_kw)
-                fig.colorbar(pos, ax=ax_bkg).ax.tick_params(labelsize=12)
+                fig.colorbar(pos, ax=ax_bkg).ax.tick_params(labelsize=self.plot_config.label_font_size)
