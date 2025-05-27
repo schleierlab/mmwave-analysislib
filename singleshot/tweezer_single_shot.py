70b08c89c1d4c31fcaf417d8e6c2a7d66a00b5bf
sslabstanford <40874785+sslabstanford@users.noreply.github.com>
2025-05-27 14:38:35 -0700

    Added show_img_only option in tweezer single shot analysis for when we want to take images for posters/ presentaiton.

--- singleshot/tweezer_single_shot.py
+++ singleshot/tweezer_single_shot.py
@@ -8,7 +8,7 @@ SHOW_ROIS = True
 SHOW_INDEX = True # site index will not show up if show_rois is set to false
 FIT_LORENTZ = False
 USE_AVERAGED_BACKGROUND = False
-
+SHOW_IMG_ONLY = False
 
 # Initialize analysis with background ROI and standard ROI loading
 tweezer_preproc = TweezerPreprocessor(
@@ -17,13 +17,14 @@ tweezer_preproc = TweezerPreprocessor(
     use_averaged_background = USE_AVERAGED_BACKGROUND
 )
 
-fig = plt.figure(layout='constrained', figsize=(10, 4))
-# fig = plt.figure(layout='constrained')
-subfigs = fig.subfigures(nrows=1, ncols=2, wspace=0.07)
-
+fig = plt.figure(layout='constrained')
 processed_results_fname = tweezer_preproc.process_shot(use_global_threshold = True)
-tweezer_preproc.show_image(roi_patches=SHOW_ROIS, site_index = SHOW_INDEX, fig=subfigs[0], vmax=100)
-# tweezer_preproc.show_image(roi_patches=SHOW_ROIS, site_index = SHOW_INDEX, fig=fig, vmax=100)
+if SHOW_IMG_ONLY:
+    tweezer_preproc.show_image(roi_patches=SHOW_ROIS, site_index = SHOW_INDEX, fig=fig, vmax=100)
+else:
+    subfigs = fig.subfigures(nrows=1, ncols=2, wspace=0.07)
+    tweezer_preproc.show_image(roi_patches=SHOW_ROIS, site_index = SHOW_INDEX, fig=subfigs[0], vmax=100)
+
 target_array = tweezer_preproc.target_array
 
 # Initialize statistician with consistent styling
@@ -33,10 +34,10 @@ tweezer_statistician = TweezerStatistician(
     plot_config=PlotConfig(),
 )
 
-if bool(tweezer_preproc.globals['do_rearrangement']):
-    tweezer_statistician.plot_target_sites_success_rate(target_array, fig = subfigs[1])
-    # pass
-else:
-    tweezer_statistician.plot_survival_rate(fig=subfigs[1], plot_lorentz = FIT_LORENTZ)
+if not SHOW_IMG_ONLY:
+    if bool(tweezer_preproc.globals['do_rearrangement']):
+        tweezer_statistician.plot_target_sites_success_rate(target_array, fig = subfigs[1])
+    else:
+        tweezer_statistician.plot_survival_rate(fig=subfigs[1], plot_lorentz = FIT_LORENTZ)
 
 #tweezer_statistician.plot_survival_rate_by_site(fig=subfigs[1])
