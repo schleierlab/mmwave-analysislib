5629cd7c93bd38e21fa28872d4942c7f81cbeb45
sslabstanford <40874785+sslabstanford@users.noreply.github.com>
2025-06-04 17:37:39 -0700

    Added comments/ change globals settings

--- multishot/tweezer_multishot_analysis.py
+++ multishot/tweezer_multishot_analysis.py
@@ -14,8 +14,8 @@ Load data directly from 'tweezer_preprocess.h5' (this is the file generated afte
 '''
 background_subtract = True
 USE_AVERAGED_BACKGROUND = True
-PLOT_AVERAGED_IMAGES = True # Show averaged image(s) but takes longer time because need to process through all h5 files
-PLOT_TWO_IMAGES = True
+PLOT_AVERAGED_IMAGES = False # Show averaged image(s) but takes longer time because need to process through all h5 files
+PLOT_TWO_IMAGES = True # only matters if PLOT_AVERAGED_IMAGES is set to True. If set to False, only plot the first image.
 folder = select_data_directory()
 
 tweezer_preproc = TweezerPreprocessor(
