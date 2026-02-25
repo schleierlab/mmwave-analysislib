a3607bed672084a6f4b0658a341392960c2e77d1
sslabstanford <40874785+sslabstanford@users.noreply.github.com>
2026-02-24 16:06:38 -0800

    added tweezer spacing analysis

--- singleshot/tweezer_beam_analysis.py
+++ singleshot/tweezer_beam_analysis.py
@@ -36,7 +36,7 @@ fitted_upopts = np.array([
     for roi in site_rois
 ])
 
-fig, axs = plt.subplots(nrows=2, layout='constrained', sharex=True)
+fig, axs = plt.subplots(nrows=3, layout='constrained', sharex=True)
 x_uopt, y_uopt, width_uopt, amp_uopt, offset_uopt = fitted_upopts.T
 # integrated_counts_u = 2 * pi * width_uopt**2 * amp_uopt
 
@@ -53,8 +53,32 @@ axs[1].errorbar(
     unp.nominal_values(waists_um_u),
     unp.std_devs(waists_um_u),
 )
+
+mean_waist_um_u = np.mean(waists_um_u)
+axs[1].axhline(
+    mean_waist_um_u.n,
+    label=f'Mean waist: {mean_waist_um_u:S} $\mu$m',
+)
 axs[1].set_ylabel(R'Atom plane waist $w$ ($\mu$m)')
 
+distances = unp.sqrt((x_uopt[1:] - x_uopt[:-1])**2 + (y_uopt[1:] - y_uopt[:-1])**2)
+distances_um_u = 1e+6 * distances * manta_tweezer_system.atom_plane_pixel_size
+
+axs[2].errorbar(
+    np.arange(site_number-1) + 0.5,
+    unp.nominal_values(distances_um_u),
+    unp.std_devs(distances_um_u),   
+)
+
+mean_distance_um_u = np.mean(distances_um_u)
+axs[2].axhline(
+    mean_distance_um_u.n,
+    label=f'Mean distance: {mean_distance_um_u:S} $\mu$m',
+)
+axs[2].set_ylabel(R'Atom plane neighbor distance $d$ ($\mu$m)')
+
 axs[-1].set_xlabel('Tweezer site')
+for ax in axs[1:]:
+    ax.legend()
 
 fig.align_labels()
